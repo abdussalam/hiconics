8f11c9ba4280313201e2ae0b5fac7de556006c9d
abdussalam <abdus.salam@gmail.com>
2026-09-07 11:30:09 +1000

    Update config_flow.py

--- custom_components/hiconics/config_flow.py
+++ custom_components/hiconics/config_flow.py
@@ -2,6 +2,7 @@
 
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import SolarmanAPIClient
@@ -61,3 +62,30 @@ class HiconicsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return HiconicsOptionsFlowHandler(config_entry)
+
+
+class HiconicsOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle Hiconics options."""
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        # Pull existing config or fallback to original setup data
+        schema = vol.Schema({
+            vol.Required(CONF_USERNAME, default=self.config_entry.options.get(CONF_USERNAME, self.config_entry.data.get(CONF_USERNAME))): str,
+            vol.Required(CONF_PASSWORD, default=self.config_entry.options.get(CONF_PASSWORD, self.config_entry.data.get(CONF_PASSWORD))): str,
+            vol.Required(CONF_APP_SECRET, default=self.config_entry.options.get(CONF_APP_SECRET, self.config_entry.data.get(CONF_APP_SECRET))): str,
+            vol.Required(CONF_SCAN_INTERVAL, default=self.config_entry.options.get(CONF_SCAN_INTERVAL, self.config_entry.data.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL))): int,
+        })
+        
+        return self.async_show_form(step_id="init", data_schema=schema)
