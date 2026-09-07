ff46b641f4db08a4f437afd888c941b093ff2b6f
abdussalam <abdus.salam@gmail.com>
2026-09-07 17:27:28 +1000

    Update const.py

--- custom_components/hiconics/const.py
+++ custom_components/hiconics/const.py
@@ -1,25 +1,13 @@
-"""Constants for the Hiconics integration."""
+"""Constants for Hiconics Solarman integration."""
 
 DOMAIN = "hiconics"
 
-# Defaults
-DEFAULT_SCAN_INTERVAL = 600  # 10 minutes
-DEFAULT_PRODUCT = "0_1067_1"
-DEFAULT_CODE_GROUP = "G1200"
-
-# Configuration keys
-CONF_APP_ID = "app_id"
-CONF_APP_SECRET = "app_secret"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
+CONF_APP_ID = "app_id"
+CONF_APP_SECRET = "app_secret"
 CONF_DEVICE_SN = "device_sn"
 CONF_DEVICE_ID = "device_id"
-CONF_PRODUCT = "product"
-CONF_CODE_GROUP = "code_group"
 CONF_SCAN_INTERVAL = "scan_interval"
 
-# API Endpoints
-URL_TOKEN = "https://globalapi.solarmanpv.com/account/v1.0/token"
-URL_DEVICE_DATA = "https://globalapi.solarmanpv.com/device/v1.0/currentData"
-URL_COMMAND_SEND = "https://globaldc-pro.solarmanpv.com/order-s/order/action/control/send"
-URL_ORDER_STATUS = "https://globaldc-pro.solarmanpv.com/order-s/order/action"
+DEFAULT_SCAN_INTERVAL = 600
