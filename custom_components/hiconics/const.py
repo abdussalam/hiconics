1ea220a65fbd3a0553f6772b73f4eb2512c4083c
abdussalam <abdus.salam@gmail.com>
2026-09-08 10:35:58 +1000

    Update const.py

--- custom_components/hiconics/const.py
+++ custom_components/hiconics/const.py
@@ -11,3 +11,12 @@ CONF_DEVICE_ID = "device_id"
 CONF_SCAN_INTERVAL = "scan_interval"
 
 DEFAULT_SCAN_INTERVAL = 600
+
+# NEW CONFIGURATION CONSTANTS
+CONF_API_DOMAIN = "api_domain"
+CONF_PRODUCT_CODE = "product_code"
+CONF_CODE_GROUP = "code_group"
+
+DEFAULT_API_DOMAIN = "globaldc-pro.solarmanpv.com"
+DEFAULT_PRODUCT_CODE = "0_1067_1"
+DEFAULT_CODE_GROUP = "G1200"
