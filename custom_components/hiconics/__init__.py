027e301fbe427e9338e4da59754a622704995811
abdussalam <abdus.salam@gmail.com>
2026-09-08 12:10:57 +1000

    Update __init__.py

--- custom_components/hiconics/__init__.py
+++ custom_components/hiconics/__init__.py
@@ -15,7 +15,7 @@ from .const import DOMAIN, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
 _LOGGER = logging.getLogger(__name__)
 
 # Re-added "button" to the load list
-PLATFORMS = ["sensor", "number", "select", "switch", "button"]
+PLATFORMS = ["sensor", "number", "select", "switch", "button", "time"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
